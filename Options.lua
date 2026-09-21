634f1c290d036aaa745d9bd30035f4f249cf0192
Mike "Xodiv" Battersby <mib@post.com>
2026-09-21 19:25:35 +1000

    Go back to AceConfig
    The Blizzard settings are tainting the actionbars!
    
    Revert "Don't require all of AceConfig just for 3 checkboxes"
    
    This reverts commit 52aa8d817d1a37436104e9adfe1f098f75292f72.

--- Options.lua
+++ Options.lua
@@ -7,9 +7,6 @@
 
 local addonName, addon = ...
 
-
---[[------------------------------------------------------------------------]]--
-
 local defaults = {
     global = {
     },
@@ -22,83 +19,56 @@ local defaults = {
     },
 }
 
-
---[[------------------------------------------------------------------------]]--
-
-addon.CheckboxControlMixin = CreateFromMixins(SettingsCheckboxControlMixin)
-
-function addon.CheckboxControlMixin:Init(initializer)
-    SettingsCheckboxControlMixin.Init(self, initializer)
-
-    local leftPad = self:GetIndent()
-
-    self.Checkbox:ClearAllPoints()
-    self.Checkbox:SetPoint("LEFT", self, "LEFT", leftPad, 0)
-
-    leftPad = leftPad + self.Checkbox:GetWidth() + 8
-    self.Text:ClearAllPoints()
-    self.Text:SetPoint("LEFT", self, "LEFT", leftPad, 0)
-end
-
-
---[[------------------------------------------------------------------------]]--
-
-local function Register(category, layout)
-
-    -- enableActionBars
-    do
-        local setting = Settings.RegisterProxySetting(
-            category,
-            "ABHIEnableActionBars",
-            Settings.VarType.Boolean,
-            "Highlight buttons on action bars.",
-            function () return defaults.profile.enableActionBars end,
-            function () return addon.db.profile.enableActionBars end,
-            function (v)
-                addon.db.profile.enableActionBars = v
-                addon.db.callbacks:Fire('OnOptionsChanged')
-            end
-        )
-        local initializer = Settings.CreateControlInitializer("ABIHCheckboxControlTemplate", setting)
-        layout:AddInitializer(initializer)
-    end
-
-    -- enableCooldownManager
-    do
-        local setting = Settings.RegisterProxySetting(
-            category,
-            "ABHIEnableCooldownManager",
-            Settings.VarType.Boolean,
-            "Highlight buttons on cooldown manager bars.",
-            function () return defaults.profile.enableCooldownManager end,
-            function () return addon.db.profile.enableCooldownManager end,
-            function (v)
-                addon.db.profile.enableCooldownManager = v
-                addon.db.callbacks:Fire('OnOptionsChanged')
-            end
-        )
-        local initializer = Settings.CreateControlInitializer("ABIHCheckboxControlTemplate", setting)
-        layout:AddInitializer(initializer)
-    end
-
-    -- enableTimer
-    do
-        local setting = Settings.RegisterProxySetting(
-            category,
-            "ABHIEnableTimer",
-            Settings.VarType.Boolean,
-            "Also show timer on highlighted buttons.",
-            function () return defaults.profile.enableTimer end,
-            function () return addon.db.profile.enableTimer end,
-            function (v)
-                addon.db.profile.enableTimer = v
-                addon.db.callbacks:Fire('OnOptionsChanged')
-            end
-        )
-        local initializer = Settings.CreateControlInitializer("ABIHCheckboxControlTemplate", setting)
-        layout:AddInitializer(initializer)
-    end
-end
+local options = {
+    type = "group",
+    args = {
+        enableActionBars = {
+            type = "toggle",
+            name = "Highlight buttons on action bars.",
+            width = "full",
+            order = 10,
+            get =
+                function ()
+                    return addon.db.profile.enableActionBars
+                end,
+            set =
+                function (_, v)
+                    addon.db.profile.enableActionBars = v
+                    addon.db.callbacks:Fire('OnOptionsChanged')
+                end,
+        },
+        enableCooldownManager = {
+            type = "toggle",
+            name = "Highlight buttons on cooldown manager bars.",
+            width = "full",
+            order = 20,
+            get =
+                function ()
+                    return addon.db.profile.enableCooldownManager
+                end,
+            set =
+                function (_, v)
+                    addon.db.profile.enableCooldownManager = v
+                    addon.db.callbacks:Fire('OnOptionsChanged')
+                end,
+        },
+        enableTimer = {
+            type = "toggle",
+            name = "Also show timer on highlighted buttons.",
+            width = "full",
+            order = 30,
+            get =
+                function ()
+                    return addon.db.profile.enableTimer
+                end,
+            set =
+                function (_, v)
+                    addon.db.profile.enableTimer = v
+                    addon.db.callbacks:Fire('OnOptionsChanged')
+                end,
+        },
+    },
+}
 
 function addon.InitializeOptions()
     addon.db = LibStub("AceDB-3.0"):New("ActionBarInterruptHighlightDB", defaults, true)
@@ -109,17 +79,10 @@ function addon.InitializeOptions()
     addon.db.RegisterCallback(addon, 'OnProfileReset', refire)
     addon.db.RegisterCallback(addon, 'OnProfileCopied', refire)
 
-    local title = C_AddOns.GetAddOnTitle(addonName)
-    local category, layout = Settings.RegisterVerticalLayoutCategory(title)
-    SettingsRegistrar:AddRegistrant(function () Register(category, layout) end)
-    Settings.RegisterAddOnCategory(category)
+    local AceConfig = LibStub("AceConfig-3.0")
+    local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 
-    SlashCmdList[addonName] =
-        function ()
-            if not InCombatLockdown() then
-                SettingsPanel:Open()
-                SettingsPanel:SelectCategory(category, true)
-            end
-        end
-    _G["SLASH_"..addonName.."1"] = "/abih"
+    local title = C_AddOns.GetAddOnTitle(addonName)
+    AceConfig:RegisterOptionsTable(title, options)
+    AceConfigDialog:AddToBlizOptions(title)
 end
