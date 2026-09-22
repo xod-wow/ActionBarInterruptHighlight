39e1f74984224b4043859b2eec9cc126687b9b46
Mike "Xodiv" Battersby <mib@post.com>
2026-09-22 14:58:16 +1000

    Update Forever interrupts (not complete)

--- SpellData/Camelot.lua
+++ SpellData/Camelot.lua
@@ -9,10 +9,11 @@ local _, addon = ...
 
 addon.Interrupts = {
     [  2139] = true,                -- Counterspell (Mage)
+    [  8042] = true,                -- Earth Shock (Shaman)
     [  1766] = true,                -- Kick (Rogue)
     [  6552] = true,                -- Pummel (Warrior)
-    [425609] = true,                -- Rebuke (Paladin)
-    [ 15487] = true,                -- Silence (Priest)
-    [410176] = true,                -- Skull Bash (Druid)
+    [425609] = true,                -- Rebuke (Paladin Engrave Pants - Rebuke)
+    [    72] = true,                -- Shield Bash
+    [410176] = true,                -- Skull Bash (Druid Engrave Gloves - Skull Bash)
     [ 19647] = true,                -- Spell Lock (Warlock Felhunter Pet)
 }
